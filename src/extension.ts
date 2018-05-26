c3f67318291ecddf0cdd5cb713ee08a53cb3eb90
visualzoran <39418724+visualzoran@users.noreply.github.com>
2018-05-26 21:14:19 +0100

    Initial code upload

---
+++ src/extension.ts
@@ -0,0 +1,26 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+    var filetemplatesExt = vscode.extensions.getExtension('visualzoran.vz-file-templates');    
+    if (filetemplatesExt) {
+        if (!filetemplatesExt.isActive)
+            filetemplatesExt.activate().then((val) => {
+                val.exports.registerTemplatesFolder(context.asAbsolutePath('templates'));
+            });
+        filetemplatesExt.exports.registerTemplatesFolder(context.asAbsolutePath('templates'));
+    }
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "vz-templates-sample-ext" is now active!');
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
