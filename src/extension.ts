469fb5e91b03b05121dd507bb9c9dafa5a641d60
visualzoran <39418724+visualzoran@users.noreply.github.com>
2018-08-26 15:48:34 +0100

    vz-file-templates api file replaced with npm package

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,7 @@
 // Import the module and reference it with the alias vscode in your code below
 
 import * as vscode from 'vscode';
+import * as vzFileTemplates from 'vz-file-templates';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
